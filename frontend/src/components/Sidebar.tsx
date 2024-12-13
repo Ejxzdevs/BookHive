2b85c492b760321bc5cf3c3210a5c77b5686f59e
Ejxzdevs <ejhayyygofredo08@gmail.com>
2024-12-14 04:05:14 +0800

    added interface

---
+++ frontend/src/components/Sidebar.tsx
@@ -0,0 +1,43 @@
+import {useState} from 'react'
+
+interface Menu {
+    name: string;
+    icon: string;
+  }
+  
+
+const Menus: Menu[] = [
+    { name: 'Dashborad', icon: 'A' },
+    { name: 'Books', icon: 'A' },
+    { name: 'Settings', icon: 'A' },
+    { name: 'Logout', icon: 'A' }
+  ];
+
+const Sidebar : React.FC = () => {
+  const [open, setOpen] = useState(true)
+  
+  return (
+    <aside className={`${open ? "w-[230px]" : "w-[50px]"} duration-300 h-screen shadow-md border relative `} >
+        <header className='p-6 flex'>
+            <h1>logo</h1>
+            <button onClick={() => setOpen(!open)} className='absolute top-7 right-0 w-7 rounded-md bg-blue-600'>X</button>
+        </header>
+        <nav>
+            <ul className='cursor-pointer'>
+                {Menus.map((menu,index)=> {
+                    return (
+                    <li className='border p-4 flex gap-2' key={index}>
+                        <p>{menu.icon}</p>
+                        <span className={`${open ? "block" : "hidden" }`} >{menu.name}</span>      
+                     </li>
+                    )
+                })}
+                <li></li>
+            </ul>
+
+        </nav>
+    </aside>
+  )
+}
+
+export default Sidebar
