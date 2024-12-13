a9199e643d69a3712c71c7999a53060f9ee3f6e5
Ejxzdevs <ejhayyygofredo08@gmail.com>
2024-12-14 04:05:47 +0800

    init

---
+++ frontend/src/pages/dashboard.tsx
@@ -0,0 +1,11 @@
+
+
+const dashboard = () => {
+  return (
+    <div>
+      <p>HELLO this is a dashboard</p>
+    </div>
+  )
+}
+
+export default dashboard
---
+++ frontend/src/pages/product.tsx
@@ -0,0 +1,11 @@
+
+
+const product = () => {
+  return (
+    <div>
+      <h1>hello i am a product</h1>
+    </div>
+  )
+}
+
+export default product
