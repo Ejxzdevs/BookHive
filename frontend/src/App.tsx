1bf76e0fb65bbab8cc76142024455ff1b7b354ce
Ejxzdevs <ejhayyygofredo08@gmail.com>
2024-12-14 04:20:40 +0800

    implement Router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css'
 import Sidebar from './components/Sidebar'
-
+import Dashboard from './pages/dashboard'
+import Product from './pages/product'
+import {Routes , Route } from 'react-router-dom'
 function App() {
   
-
   return (
    <div className='flex flex-row'>
       <Sidebar/>
@@ -11,6 +12,10 @@ function App() {
         <h1 className="text-3xl font-bold underline">
           Hello world!
         </h1>
+        <Routes>
+            <Route path='/' element={<Dashboard/> }/>
+            <Route path='/product' element={<Product/> }/> 
+        </Routes>
       </main>
    </div>
   )
