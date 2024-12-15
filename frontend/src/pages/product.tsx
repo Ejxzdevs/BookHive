6916b43ae48b4dd6d4e3d4f4c3b2a15bc0672ca4
Ejxzdevs <ejhayyygofredo08@gmail.com>
2024-12-15 15:37:41 +0800

    add mui table

--- frontend/src/pages/product.tsx
+++ frontend/src/pages/product.tsx
@@ -1,9 +1,12 @@
-import {useState , useEffect } from 'react';
+import React , {useState , useEffect } from 'react';
 import axios from 'axios';
+import { Container , Box, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Typography , Button } from '@mui/material';
 
 interface books {
+  book_id: number
   book_title: string
   book_description: string
+  book_release: Date
 }
 const Product : React.FC= () => {
 
@@ -14,6 +17,7 @@ const Product : React.FC= () => {
     axios.get('http://localhost:8080/books')
       .then((response) => {
         setBooks(response.data);
+        console.log(response.data);
       })
       .catch((err) => {
         console.log(err)
@@ -22,18 +26,44 @@ const Product : React.FC= () => {
 
 
   return (
-    <div>
-      <ul>
-      {books.map((book,index)=>{
+    <Container >
+      <Box>
+        <Typography variant="body1" color="initial">
+          Hello
+        </Typography>
+      </Box>
+      <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Book Title</TableCell>
+            <TableCell>Book Title</TableCell>
+            <TableCell>Book Description</TableCell>
+            <TableCell>Book Author</TableCell>
+            <TableCell>Book Release</TableCell>
+            <TableCell>Action</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+      {books.map((book)=>{
         return (
-          <li key={index} >
-              <p>{book.book_title}</p>
-              <span>{book.book_description}</span>  
-          </li>
+            <TableRow key={book.book_id}>
+              <TableCell className='' >{book.book_id}</TableCell>
+              <TableCell className='' >{book.book_title}</TableCell>
+              <TableCell>{book.book_description}</TableCell>
+              <TableCell>{book.book_description}</TableCell>
+              <TableCell>{new Date(book.book_release).toLocaleDateString()}</TableCell>
+              <TableCell>
+                    <Button variant="contained" color="primary" >edit</Button>
+                    <Button variant="outlined" color="secondary" >delete</Button>
+            </TableCell>
+          </TableRow>
         )
       })}
-      </ul>
-    </div>
+       </TableBody>
+      </Table>
+    </TableContainer>
+    </Container>
   )
 }
 
