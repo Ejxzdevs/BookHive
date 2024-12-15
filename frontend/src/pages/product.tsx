8b12b749a90ad1c8e1cb5a5738ef458b2b2b546f
Ejxzdevs <ejhayyygofredo08@gmail.com>
2024-12-16 01:07:09 +0800

    init

--- frontend/src/pages/product.tsx
+++ frontend/src/pages/product.tsx
@@ -1,70 +1,37 @@
-import React , {useState , useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Container , Box, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Typography , Button } from '@mui/material';
+import Container from '@mui/material/Container';
+import TableData from '../components/tables/TableData';
 
-interface books {
-  book_id: number
-  book_title: string
-  book_description: string
-  book_release: Date
+interface Book {
+  book_id: number;
+  book_title: string;
+  book_description: string;
+  book_release: Date;
+  [key: string]: string | number | Date;  // Allow flexible keys
 }
-const Product : React.FC= () => {
 
-  const [books, setBooks] = useState<books[]>([]);
+const Product: React.FC = () => {
+  const [books, setBooks] = useState<Book[]>([]);
+  const headers: Array<string> = ['Id', 'Book Title', 'Description', 'Author', 'Date Release'];
 
   useEffect(() => {
-    // Fetch books from the API
-    axios.get('http://localhost:8080/books')
+    axios
+      .get('http://localhost:8080/books')
       .then((response) => {
         setBooks(response.data);
         console.log(response.data);
       })
       .catch((err) => {
-        console.log(err)
+        console.log(err);
       });
-  }, []);
-
+  },[]);
 
   return (
-    <Container >
-      <Box>
-        <Typography variant="body1" color="initial">
-          Hello
-        </Typography>
-      </Box>
-      <TableContainer component={Paper}>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>Book Title</TableCell>
-            <TableCell>Book Title</TableCell>
-            <TableCell>Book Description</TableCell>
-            <TableCell>Book Author</TableCell>
-            <TableCell>Book Release</TableCell>
-            <TableCell>Action</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-      {books.map((book)=>{
-        return (
-            <TableRow key={book.book_id}>
-              <TableCell className='' >{book.book_id}</TableCell>
-              <TableCell className='' >{book.book_title}</TableCell>
-              <TableCell>{book.book_description}</TableCell>
-              <TableCell>{book.book_description}</TableCell>
-              <TableCell>{new Date(book.book_release).toLocaleDateString()}</TableCell>
-              <TableCell>
-                    <Button variant="contained" color="primary" >edit</Button>
-                    <Button variant="outlined" color="secondary" >delete</Button>
-            </TableCell>
-          </TableRow>
-        )
-      })}
-       </TableBody>
-      </Table>
-    </TableContainer>
+    <Container>
+      <TableData headers={headers} data={books} />
     </Container>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
