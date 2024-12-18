cd55311ceef7a42ec82af6311339060babd488a6
Ejxzdevs <ejhayyygofredo08@gmail.com>
2024-12-18 22:54:04 +0800

    fixed responsiveness

--- frontend/src/components/main/HeroSection.tsx
+++ frontend/src/components/main/HeroSection.tsx
@@ -3,15 +3,26 @@ import LibraryImage from '../../assets/Library.jpg';
 
 const HeroSection = () => {
   return (
-    <Box className="relative h-[100vh] bg-cover bg-center"
+    <Box className="
+    relative h-auto bg-cover bg-center py-32 px-5 
+    sm:h-auto 
+    md:h-[100vh] 
+    md:flex
+    md:items-center
+    md:justify-center
+    lg:h-[100vh]
+    lg:flex
+    lg:items-center
+    lg:justify-center
+    "
       sx={{
         backgroundImage: `url(${LibraryImage})`,
       }} >
 
       {/* FOR OVERLAY */}
       <Box className="absolute top-0 left-0 right-0 bottom-0 bg-black opacity-70"></Box>
-      <Box className="relative z-10 text-white flex justify-center items-center min-h-full px-32">
-        <Typography variant="body1">Welcome to [Your System Name], where managing your library is made easier and more efficient. Our system offers key advantages such as seamless book cataloging, real-time loan tracking, streamlined user management, and simplified inventory control. With a user-friendly interface and automated processes, [Your System Name] ensures improved organization, faster operations, and enhanced accessibility for both library staff and patrons</Typography>
+      <Box className="relative z-10 text-white h-auto max-w-[800px]">
+        <Typography  variant="body2">Welcome to [Your System Name], where managing your library is made easier and more efficient. Our system offers key advantages such as seamless book cataloging, real-time loan tracking, streamlined user management, and simplified inventory control. With a user-friendly interface and automated processes, [Your System Name] ensures improved organization, faster operations, and enhanced accessibility for both library staff and patrons</Typography>
       </Box>
 
     </Box>
