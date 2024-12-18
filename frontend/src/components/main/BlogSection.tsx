202ad5fd569ecc4dcb4bffd4b41ef5174c3c3184
Ejxzdevs <ejhayyygofredo08@gmail.com>
2024-12-18 15:20:20 +0800

    init

---
+++ frontend/src/components/main/BlogSection.tsx
@@ -0,0 +1,11 @@
+import { Box , Typography } from "@mui/material"
+
+const BlogSection = () => {
+  return (
+    <Box className="h-[100vh] ">
+       <Typography variant="body1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, vitae itaque nisi sunt beatae natus. Sit doloremque modi quod vitae nihil suscipit minima quasi voluptas id, eaque aspernatur aliquid rem.</Typography>
+    </Box>
+  )
+}
+
+export default BlogSection
--- frontend/src/components/main/HeroSection.tsx
+++ frontend/src/components/main/HeroSection.tsx
@@ -1,5 +1,5 @@
 import { Box, Typography } from "@mui/material";
-import LibraryImage from '../../assets/Library.jpg'; // Correct import if the image is inside src/assets/
+import LibraryImage from '../../assets/Library.jpg';
 
 const HeroSection = () => {
   return (
@@ -13,7 +13,7 @@ const HeroSection = () => {
       <Box className="relative z-10 text-white flex justify-center items-center min-h-full px-32">
         <Typography variant="body1">Welcome to [Your System Name], where managing your library is made easier and more efficient. Our system offers key advantages such as seamless book cataloging, real-time loan tracking, streamlined user management, and simplified inventory control. With a user-friendly interface and automated processes, [Your System Name] ensures improved organization, faster operations, and enhanced accessibility for both library staff and patrons</Typography>
       </Box>
-      
+
     </Box>
   );
 };
