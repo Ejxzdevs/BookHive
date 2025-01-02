1877ca1ae07791a1a0b875f661142e65aac86e2b
Ejxzdevs <ejhayyygofredo08@gmail.com>
2025-01-03 02:57:58 +0800

    init

--- frontend/src/pages/dashboard.tsx
+++ frontend/src/pages/dashboard.tsx
@@ -1,8 +1,9 @@
-
+import Sidebar from "../components/Sidebar"
 
 const Dashboard = () => {
   return (
-    <div>
+    <div className="h-[100vh] flex flex-row ">
+      <Sidebar/> 
       <p>HELLO this is a dashboard</p>
     </div>
   )
