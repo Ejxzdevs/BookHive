e688bd21f956c03dada8e29dae285fdec7db96e1
Ejxzdevs <ejhayyygofredo08@gmail.com>
2025-01-03 01:34:22 +0800

    change btn-color

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,9 +1,54 @@
+import { Box, Typography, Button, TextField } from '@mui/material';
+import LibraryImage from '../assets/Library.jpg';
+
 const Login = () => {
   return (
-    <div>
-      <h1>i am login</h1>
-    </div>
-  )
-}
+    <Box 
+      className="relative flex items-center justify-center h-[100vh] bg-cover bg-center" 
+      sx={{
+        backgroundImage: `url(${LibraryImage})`,
+      }}
+    >
+      {/* Overlay to dim the background */}
+      <Box
+        className="absolute inset-0 bg-black opacity-50" // This creates the dimming effect
+        sx={{
+          zIndex: 1,
+        }}
+      />
 
-export default Login
+      <form
+        className="relative flex flex-col gap-5 border border-gray-500 h-auto w-[500px] bg-white opacity-90 rounded-md px-5 py-20 z-10"
+      >
+        <Box className="text-dark h-auto max-w-[800px]">
+          <Typography
+            sx={{
+              fontSize: { xs: '1rem', sm: '2rem', md: '3rem' },
+            }}
+
+            
+            className='text-center'
+          >
+            Contact Us
+          </Typography>
+        </Box>
+        <TextField
+          id="email"
+          label="Email"
+          fullWidth
+        />
+        <TextField
+          id="password"
+          label="Password"
+          type="password"
+          fullWidth
+        />
+        <Button type="submit" variant="contained" fullWidth   sx={{ backgroundColor: '#19B37E' }} >
+          Submit
+        </Button>
+      </form>
+    </Box>
+  );
+};
+
+export default Login;
