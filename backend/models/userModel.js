8d2c6e048b6f5f3afbc359153841a5e645670549
Ejxzdevs <ejhayyygofredo08@gmail.com>
2025-01-03 11:20:09 +0800

    change lowercase table name

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -3,7 +3,7 @@ const connect = require('../config/db');
 class Account {
 
     static async getUser(data) {
-        const query = 'SELECT * FROM ACCOUNT WHERE user_email = ? AND user_password = ?';
+        const query = 'SELECT * FROM account WHERE user_email = ? AND user_password = ?';
         try {
             const [results] = await connect.promise().query(query, [data.user_email, data.user_password]);
             return results;
