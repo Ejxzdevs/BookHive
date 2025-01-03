75bd73ac324f22a2f859eaeb6644bfa438748c8d
Ejxzdevs <ejhayyygofredo08@gmail.com>
2025-01-03 23:16:26 +0800

    add useLocation for condition

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,7 @@ import EnduserAbout from './pages/EnduserAbout';
 import EnduserContact from './pages/EnduserContact';
 import Navigation from "./components/Navigation";
 import Dashboard from './pages/dashboard';
+import Product from './pages/product';
 import Footer from './components/Footer';
 import Login from './pages/Login';
 import { useState, useEffect } from 'react';
@@ -37,6 +38,7 @@ function App() {
           <Route path="/Contact" element={<EnduserContact />} />
           <Route path="/admin" element={<Login />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/product" element={<Product />} />
         </Routes>
       </Box>
       
