e7f46083b6832821ed2c0efc547946363dc11e50
Ejxzdevs <ejhayyygofredo08@gmail.com>
2025-01-04 17:43:24 +0800

    used FormData

---
+++ frontend/src/services/bookApi.tsx
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+const apiClient = axios.create({
+  baseURL: 'http://localhost:8080',
+});
+
+export const insertBook = async (data: { book_title: string; book_description: string; image_url: File; }) => {
+  try {
+
+    const formData = new FormData();
+    formData.append('book_title', data.book_title);
+    formData.append('book_description', data.book_description);
+    
+    // Append the file to the FormData object
+    formData.append('image_url', data.image_url);
+    const response = await apiClient.post('/books/add', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    });
+
+    return response.data; 
+  } catch (error) {
+    console.error('Error inserting book:', error);
+    throw error; 
+  }
+};
