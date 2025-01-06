d10248e7b8379faf2f3cc2f234b40a29e782dec2
Ejxzdevs <ejhayyygofredo08@gmail.com>
2025-01-06 19:51:53 +0800

    change type

--- frontend/src/pages/product.tsx
+++ frontend/src/pages/product.tsx
@@ -1,43 +1,55 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import { Container, Box } from "@mui/material";
 import TableData from '../components/tables/TableData';
 import Sidebar from '../components/Sidebar';
-import { Container, Box  } from "@mui/material"
 import Header from '../components/Header';
-import AddBook from '../components/modals/AddBook'
+import AddBook from '../components/modals/AddBook';
+import { getAllBooks } from '../services/bookApi';
+
 interface Book {
   book_id: number;
   book_title: string;
   genre: string;
   author: string;
+  book_description: string;
   book_release: Date;
+  image_url: string;
   [key: string]: string | number | Date;
 }
 
-  const Product: React.FC = () => {
+const Product: React.FC = () => {
   const [books, setBooks] = useState<Book[]>([]);
   const headers: Array<string> = ['Id', 'Title', 'Genre', 'Author', 'Date Added', 'Action'];
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
-    axios
-      .get('http://localhost:8080/books')
-      .then((response) => {
-        setBooks(response.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  },[]);
+    const fetchBooks = async () => {
+      try {
+        const data = await getAllBooks();
+        setBooks(data);
+        setLoading(false);
+      } catch (err) {
+        setError('Error fetching books');
+        setLoading(false);
+        console.error('Error fetching books:', err);
+      }
+    };
+
+    fetchBooks();
+  }, []);
 
   return (
     <Container disableGutters maxWidth={false} className="h-[100vh] flex flex-row p-0 m-0">
-      <Sidebar/>
-      <Box className="flex flex-grow flex-col  ">
-        <Header/>
+      <Sidebar />
+      <Box className="flex flex-grow flex-col">
+        <Header />
         <Box className="flex justify-end pt-5 pe-6">
-          <AddBook/>
+          <AddBook />
         </Box>
-          <TableData headers={headers} data={books} />
+        {loading && <p>Loading...</p>}
+        {error && <p>{error}</p>}
+        {!loading && !error && <TableData headers={headers} data={books} />}
       </Box>
     </Container>
   );
