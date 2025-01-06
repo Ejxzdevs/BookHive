16aa5d9a95d2a5e78ffb0fb52e424dcee587c528
Ejxzdevs <ejhayyygofredo08@gmail.com>
2025-01-07 03:19:35 +0800

    added delete method

--- backend/controllers/bookController.js
+++ backend/controllers/bookController.js
@@ -25,7 +25,7 @@ class BookController {
       res.status(500).json({ error: err.message });
     }
   };
-
+  // UPDATE BOOK
   updatebook = async (req , res) => {
     const { book_title, book_description , genre , author, } = req.body;
     let image_url = (req.file && req.file.filename) ? `${req.file.filename}` : '';
@@ -38,6 +38,19 @@ class BookController {
     } catch (err) {
       res.status(500).json({ error: err.message });
     }
+  }
+
+  // DELETE BOOK
+  deleteBook = async (req , res) => {
+    const book_id = req.params.id
+
+    try {
+      const result = await BookModel.deleteBook(book_id);
+      res.status(200).json({ message: 'Book deleted successfully', result });
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+
   }
 }
 
