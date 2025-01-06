f98b5bc43bc0240233bf09c2cf49ff4e921052be
Ejxzdevs <ejhayyygofredo08@gmail.com>
2025-01-06 19:45:48 +0800

    added update api

--- frontend/src/services/bookApi.tsx
+++ frontend/src/services/bookApi.tsx
@@ -4,6 +4,16 @@ const apiClient = axios.create({
   baseURL: 'http://localhost:8080',
 });
 
+export const getAllBooks = async () => {
+  try {
+    const response = await apiClient.get('/books');
+    return response.data; 
+  } catch (error) {
+    console.error('Error fetching books:', error);
+    throw error;
+  }
+}
+
 export const insertBook = async (data: { book_title: string; book_description: string; genre: string; author: string; image_url: File; }) => {
   try {
 
@@ -27,3 +37,23 @@ export const insertBook = async (data: { book_title: string; book_description: s
     throw error; 
   }
 };
+
+
+export const updateBook = async (data: { book_title: string; book_description: string; genre: string; author: string; image: File | null ; id: number }) => {
+  try {
+    const formData = new FormData();
+    formData.append('book_title', data.book_title);
+    formData.append('book_description', data.book_description);
+    formData.append('genre', data.genre);
+    formData.append('author', data.author);
+    formData.append('image_url', data.image ? data.image : '');
+    const response = await apiClient.patch(`/books/update/${data.id}`, formData);
+
+    return response.data; 
+  } catch (error) {
+    console.error('Error updating book:', error);
+    throw error; 
+  }
+};
+
+
