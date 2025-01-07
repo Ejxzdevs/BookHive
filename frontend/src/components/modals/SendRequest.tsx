71f77fcfef02a9ae5cb7fe8ea8f51ab4e5ff5972
Ejxzdevs <ejhayyygofredo08@gmail.com>
2025-01-08 01:47:51 +0800

    implement dynamic function

--- frontend/src/components/modals/SendRequest.tsx
+++ frontend/src/components/modals/SendRequest.tsx
@@ -2,15 +2,35 @@ import { Dialog , DialogTitle ,  DialogContent , DialogActions , TextField , But
 import CloseIcon from '@mui/icons-material/Close';
 import { useState } from "react";
 import { FormProps } from '../../types/bookInterface'
-
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { RequestSchema, RequestFormData } from '../../utils/formSchema'
+import { insertRequest } from '../../services/requestApi'
 
 const SendRequest : React.FC<FormProps> = ({ data }) => {
     const [open , setOpen] = useState<boolean>(false);
-
     const handleOpen = () => setOpen(true)
     const handleClose = () => setOpen(false)
 
-   
+    const { register, handleSubmit, formState: { errors }, } = useForm<RequestFormData>({ resolver: zodResolver(RequestSchema),});
+      const onSubmit: SubmitHandler<RequestFormData> = async (data) => {
+        try {
+          console.log("Form submitted with data: ", data);
+            const response = await insertRequest({
+                  book_id: data.book_id,
+                  fullname: data.fullname,
+                  request_email: data.request_email,
+                  phone_number: data.phone_number,
+                  request_message: data.request_message ? data.request_message : ' ' 
+            });
+          console.log('Request added successfully:',response);
+          window.location.reload();
+    
+        } catch (error) {
+          console.error('Error submitting Request:', error);
+        }
+      };
+    
 
   return (
     <Box>
@@ -27,30 +47,53 @@ const SendRequest : React.FC<FormProps> = ({ data }) => {
                 Request for {data[0].book_title}
             </DialogTitle>
             <DialogContent>
-            <form className="p-5 flex flex-col gap-5 w-[450px]" action="">
+            <form className="p-5 flex flex-col gap-5 w-[450px]" 
+            onSubmit={handleSubmit(onSubmit)}
+            >
                 <Box mt={2} display="flex" justifyContent="center">
                 <img
                   src={`http://localhost:8080/${data[0].image_url}`}
                   style={{ maxWidth: '100%', maxHeight: '200px' }}
                 />
               </Box>
+              <TextField
+                    label="book id"
+                    fullWidth
+                    value={data[0].book_id}
+                    {...register('book_id')}
+                    error={!!errors.book_id}
+                    helperText={errors.book_id?.message}
+                    sx={{ display: 'none'}}
+                />
                 <TextField
                     label="Full Name"
                     fullWidth
+                    {...register('fullname')}
+                    error={!!errors.fullname}
+                    helperText={errors.fullname?.message}
                 />
                 <TextField
                     label="Email"
                     fullWidth
+                    {...register('request_email')}
+                    error={!!errors.request_email}
+                    helperText={errors.request_email?.message}
                 />
                 <TextField
                     label="Number"
                     fullWidth
+                    {...register('phone_number')}
+                    error={!!errors.phone_number}
+                    helperText={errors.phone_number?.message}
                 />
                 <TextField
                     label="Message(Optional)"
                     multiline
                     rows={5}
                     fullWidth
+                    {...register('request_message')}
+                    error={!!errors.request_message}
+                    helperText={errors.request_message?.message}
                 />
                 <DialogActions>
                     <Button type="submit" variant="contained" fullWidth sx={{ backgroundColor: '#19B37E' }} >
@@ -65,3 +108,5 @@ const SendRequest : React.FC<FormProps> = ({ data }) => {
 }
 
 export default SendRequest
+
+
