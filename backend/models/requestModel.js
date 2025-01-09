4d35ba8a6d48f17663b7f4f94654dc240f8ab6ee
Ejxzdevs <ejhayyygofredo08@gmail.com>
2025-01-10 03:44:19 +0800

    added request method for get all requests

--- backend/models/requestModel.js
+++ backend/models/requestModel.js
@@ -1,6 +1,17 @@
 const connect = require('../config/db');
 
 class Request {
+
+    static async getAllRequest(){
+        const query = "SELECT * FROM requests INNER JOIN books ON requests.book_id = books.book_id ";
+        try {
+            const [result] = await connect.promise().query(query);
+            return result;
+        } catch (error) {
+            throw new Error('Error inserting request');
+        }
+    }
+
     static async insertRequest(request) {
         const query = "INSERT INTO REQUESTS (book_id, fullname, request_email , phone_number, request_message) VALUES (?)";
         const values = [request.book_id, request.fullname, request.request_email, request.phone_number, request.request_message];
