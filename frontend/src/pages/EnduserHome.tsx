e5d0b3ff6226cfa6c48dac859a5b2915024603db
Ejxzdevs <ejhayyygofredo08@gmail.com>
2025-01-09 17:19:14 +0800

    adde popular & latest components

--- frontend/src/pages/EnduserHome.tsx
+++ frontend/src/pages/EnduserHome.tsx
@@ -2,18 +2,25 @@ import HeroSection from "../components/main/HeroSection"
 import { Box, Typography } from "@mui/material"
 import BlogSection from "../components/main/BlogSection"
 import LatestBooks from "../components/main/LatestBooks"
+import PopularBooks from "../components/main/PopularBooks"
 
 const enduserHome = () => {
   return (
     <Box >
       <HeroSection/>
       <BlogSection/>
-      <Box className="ps-8" >
+      <Box className="ps-8 m-10" >
         <Typography variant="h3" color="initial">
           Latest Books
         </Typography>
       </Box>
       <LatestBooks/>
+      <Box className="ps-8 m-10" >
+        <Typography variant="h3" color="initial">
+          Popular Books
+        </Typography>
+      </Box>
+        <PopularBooks/>
     </Box>
   )
 }
