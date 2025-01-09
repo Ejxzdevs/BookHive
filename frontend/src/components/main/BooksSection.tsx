14bedd03612503d6c33511809a7aeed651b565ca
Ejxzdevs <ejhayyygofredo08@gmail.com>
2025-01-09 14:10:44 +0800

    implement viewBook component

--- frontend/src/components/main/BooksSection.tsx
+++ frontend/src/components/main/BooksSection.tsx
@@ -1,19 +1,18 @@
-import { Box, Button, Card, CardActions, CardContent, CardMedia, Typography, Dialog, DialogContent } from '@mui/material';
+import { Box, Card, CardActions, CardContent, CardMedia, Typography } from '@mui/material';
 import Grid from '@mui/material/Grid2';
 import LibraryImage from '../../assets/Library.jpg';
 import { getAllBooks } from '../../services/bookApi';
 import { useEffect, useState } from 'react';
 import { Book } from '../../types/bookInterface';
-import CloseIcon from '@mui/icons-material/Close';
+
 import SeachBar from '../forms/SeachBar';
 import SendRequest from '../modals/SendRequest'
+import ViewBook from '../modals/ViewBook';
 
 const BooksSection = () => {
   const [books, setBooks] = useState<Book[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
-  const [openModal, setOpenModal] = useState(false);
-  const [selectedBook, setSelectedBook] = useState<Book | null>(null);
   const [search , setSearch ] = useState<string>('');
   const searchItem = "Seacrh Title , Author or Genre";
 
@@ -32,15 +31,6 @@ const BooksSection = () => {
 
     fetchBooks();
   },[]);
-
-  const handleOpenModal = (book: Book) => {
-    setSelectedBook(book);
-    setOpenModal(true);
-  };
-
-  const handleCloseModal = () => {
-    setOpenModal(false);
-  };
 
   const filteredBooks = books.filter((book) =>
     book.book_title.toLowerCase().includes(search.toLowerCase()) ||
@@ -67,7 +57,7 @@ const BooksSection = () => {
         {!loading && !error && (
           <Grid container spacing={2}>
             {filteredBooks.map((book) => (
-              <Grid size={{ xs: 12, sm: 6, md: 3 }} key={book.book_id} container spacing={2}>
+              <Grid size={{ xs: 12, sm: 6, md: 4 }} key={book.book_id} container spacing={2}>
                 <Card sx={{ maxWidth: 330 , Height: 'auto',  boxShadow: 5, paddingY: '10px' }}>
                   <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', paddingX: '15px', paddingY: '5px' }}>
                     <CardMedia sx={{ height: 180, width: 120 , backgroundPosition: 'center' }} image={`http://localhost:8080/${book.image_url}`} title="Book Image" />
@@ -85,7 +75,7 @@ const BooksSection = () => {
                   </CardContent>
                   <CardActions sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', gap: 2 }}>
                     <SendRequest data={[book]}/>
-                    <Button color='primary' onClick={() => handleOpenModal(book)}>View</Button>
+                    <ViewBook data={[book]} />
                   </CardActions>
                 </Card>
               </Grid>
@@ -93,51 +83,6 @@ const BooksSection = () => {
           </Grid>
         )}
       </Box>
-
-      {/* Modal for book details */}
-      <Dialog open={openModal}>
-        <Box className={`flex justify-end items-center`} >
-            <Button color='error' onClick={handleCloseModal} sx={{margin: 0}} >
-                <CloseIcon/>
-            </Button>
-        </Box>
-        <Box className="pl-[22px]" sx={{ margin: '0' }} >
-            <Typography variant="h5" >{selectedBook?.book_title}</Typography>
-            <Typography variant="body1" component="div" sx={{  color: 'text.secondary', margin: '0' }}>
-                    Date Release: {selectedBook?.book_release
-                        ?   (
-                    isNaN(new Date(selectedBook.book_release).getTime())
-                        ? 'Invalid release date'
-                        : new Date(selectedBook.book_release).toLocaleDateString()
-                            )
-                        : 'No release date'}
-            </Typography>
-        </Box>
-        <DialogContent className='max-w-[450px]  min-h-[600px] '  >
-            <Box className="flex items-center justify-center" >
-                <img
-                className='h-[250px] w-[180px]' 
-                src={`http://localhost:8080/${selectedBook?.image_url}`}
-                alt={`http://localhost:8080/${selectedBook?.image_url}`}
-                />
-            </Box>
-            <Box className="flex flex-row justify-between items-center gap-5 mt-2 "  >
-                <Box>
-                <Typography gutterBottom variant="body1" component="div" sx={{ margin: '0' }}>
-                    Author: {selectedBook?.author}
-                </Typography>
-                <Typography variant="body2" sx={{ color: 'text.secondary', margin: '0' }}>
-                    Genre: {selectedBook?.genre}
-                </Typography>
-                </Box>
-            </Box>
-            <Box>
-                <Typography variant="body1" sx={{ color: 'text.secondary' , marginTop: '20px'}}>
-                    {selectedBook?.book_description}
-                </Typography>
-            </Box>
-        </DialogContent>
-      </Dialog>
     </Box>
   );
 };
