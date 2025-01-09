2c8273725230d1ba202b2d3f823af5369475d85e
Ejxzdevs <ejhayyygofredo08@gmail.com>
2025-01-09 17:19:39 +0800

    removed viewBook interface

--- frontend/src/types/bookInterface.tsx
+++ frontend/src/types/bookInterface.tsx
@@ -6,6 +6,8 @@ export interface Book {
     book_description: string;
     book_release: Date;
     image_url: File | string;
+    book_status: string;
+    request_count : number;
 }
   
 export interface FormProps {
