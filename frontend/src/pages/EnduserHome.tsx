4c089317e6bfba55ebba1cba44ad9b84403a4d17
Ejxzdevs <ejhayyygofredo08@gmail.com>
2025-01-09 14:16:21 +0800

    added label for latest books

--- frontend/src/pages/EnduserHome.tsx
+++ frontend/src/pages/EnduserHome.tsx
@@ -1,5 +1,5 @@
 import HeroSection from "../components/main/HeroSection"
-import { Box } from "@mui/material"
+import { Box, Typography } from "@mui/material"
 import BlogSection from "../components/main/BlogSection"
 import LatestBooks from "../components/main/LatestBooks"
 
@@ -8,6 +8,11 @@ const enduserHome = () => {
     <Box >
       <HeroSection/>
       <BlogSection/>
+      <Box className="ps-8" >
+        <Typography variant="h3" color="initial">
+          Latest Books
+        </Typography>
+      </Box>
       <LatestBooks/>
     </Box>
   )
