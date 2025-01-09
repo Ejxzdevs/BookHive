0651bfa72a9b0a2760645f5e26b9f2fc0d3f846e
Ejxzdevs <ejhayyygofredo08@gmail.com>
2025-01-10 03:45:24 +0800

    added route for get all requets

--- backend/routes/requestRoutes.js
+++ backend/routes/requestRoutes.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const requestController = require('../controllers/requestController')
 
+router.get('/', requestController.getRequests)
 router.post('/add', requestController.addRequest)
 
 module.exports = router;
