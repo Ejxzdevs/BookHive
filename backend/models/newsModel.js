9df4994eb66eccd0359c8274bf4dd383ccae675a
Ejxzdevs <ejhayyygofredo08@gmail.com>
2025-01-11 13:17:32 +0800

    init

---
+++ backend/models/newsModel.js
@@ -0,0 +1,55 @@
+const connect = require('../config/db');
+
+class News {
+
+    static async getAllBooks() {
+        const query = "SELECT books.*, COUNT(requests.request_id) AS request_count FROM books LEFT JOIN requests ON requests.book_id = books.book_id GROUP BY books.book_id;";
+        try {
+            const [results] = await connect.promise().query(query);
+            return results; 
+        } catch (error) {
+            throw new Error('Error fetching books');
+        }
+    }
+
+    static async insertNews(news) {
+        const query = "INSERT INTO news (news_title, news_content, news_image) VALUES (?)";
+        const values = [news.news_title, news.news_content ,news.image_url];
+        try {
+            const [result] = await connect.promise().query(query, [values]);
+            return result;
+        } catch (error) {
+            throw new Error('Error inserting book');
+        }
+    }
+
+    static async updateBook(book) {
+        const query = `
+        UPDATE books
+        SET book_title = ?, book_description = ?, genre = ?, author = ?, image_url = COALESCE(?, image_url)
+        WHERE book_id = ?
+        `;
+        const values = [book.book_title, book.book_description, book.genre, book.author, book.image_url ? book.image_url : null, book.book_id];
+        
+        try {
+            const [result] = await connect.promise().query(query, values);
+            return result;
+        } catch (error) {
+            console.error('Error updating book: ', error);
+            throw new Error('Error updating book');
+        }
+    }
+
+    static async deleteBook (id) {
+        const query = " DELETE FROM books WHERE book_id = ? ";
+        const value = [id];
+        try {
+            const [result] = await connect.promise().query(query,value)
+            return result;
+        } catch (error) {
+            console.error('Error deleting book: ', error);
+            throw new Error('Error deleting book');
+        }
+    }
+}
+module.exports = News;
