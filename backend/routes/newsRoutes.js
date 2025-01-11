2cb853a048609de01add10fccb60afe89affeec8
Ejxzdevs <ejhayyygofredo08@gmail.com>
2025-01-11 23:35:27 +0800

    added patch method

--- backend/routes/newsRoutes.js
+++ backend/routes/newsRoutes.js
@@ -17,6 +17,7 @@ const upload = multer({ storage: storage });
 // Route News
 router.get('/', newsController.getNews);
 router.post('/add', upload.single('image_url'), newsController.addNews);
+router.patch('/update/:id', upload.single('image_url'), newsController.updateNews);
 router.delete('/delete/:id', newsController.deleteNews);
 
 module.exports = router;
