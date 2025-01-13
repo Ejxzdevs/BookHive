e92db7dbd889021a75f36ec1966b6108b211311b
Ejxzdevs <ejhayyygofredo08@gmail.com>
2025-01-14 03:18:21 +0800

    add route for get reports

--- backend/routes/reportRoutes.js
+++ backend/routes/reportRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const reportController = require('../controllers/reportController');
 
 // Route for Report
+router.get('/', reportController.getReports);
 router.post('/add', reportController.addReport);
 
 module.exports = router;
