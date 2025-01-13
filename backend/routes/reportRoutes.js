5c7b5b24956f4020c5f4c868f2fffd904a50edd0
Ejxzdevs <ejhayyygofredo08@gmail.com>
2025-01-14 03:41:38 +0800

    add delete route

--- backend/routes/reportRoutes.js
+++ backend/routes/reportRoutes.js
@@ -5,5 +5,6 @@ const reportController = require('../controllers/reportController');
 // Route for Report
 router.get('/', reportController.getReports);
 router.post('/add', reportController.addReport);
+router.delete('/delete/:id', reportController.deleteReport);
 
 module.exports = router;
