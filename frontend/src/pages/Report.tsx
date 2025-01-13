398be402329e34da7f78b2fcc3baaf032d993a14
Ejxzdevs <ejhayyygofredo08@gmail.com>
2025-01-14 03:19:04 +0800

    implement api for getting all reports

--- frontend/src/pages/Report.tsx
+++ frontend/src/pages/Report.tsx
@@ -1,9 +1,32 @@
+import { useState , useEffect } from "react";
 import { Container, Box, Typography } from "@mui/material";
 import Sidebar from '../components/Sidebar';
 import Header from '../components/Header';
 import AddReport from "../components/modals/AddReport";
+import ReportData from "../components/tables/ReportData";
+import { getAllReport } from "../services/reportApi"
+import type { Report } from '../types/reportInterface'
 
 const Product: React.FC = () => {
+    const [reports, setReports] = useState<Report[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        const fetchBooks = async () => {
+          try {
+            const data = await getAllReport();
+            setReports(data);
+            setLoading(false);
+          } catch (err) {
+            setError('Error fetching reports');
+            setLoading(false);
+            console.error('Error fetching reports:', err);
+          }
+        };
+    
+        fetchBooks();
+      }, []);
 
 
   return (
@@ -17,6 +40,9 @@ const Product: React.FC = () => {
           </Typography>
           <AddReport/>
         </Box>
+        {loading && <p>Loading...</p>}
+        {error && <p>{error}</p>}
+        {!loading && !error && <ReportData data={reports} />}
       </Box>
     </Container>
   );
