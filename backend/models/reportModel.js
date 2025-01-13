04b676107234fd4235dcca3b136126fd65cf4fda
Ejxzdevs <ejhayyygofredo08@gmail.com>
2025-01-14 03:41:13 +0800

    create delete query

--- backend/models/reportModel.js
+++ backend/models/reportModel.js
@@ -2,6 +2,7 @@ const connect = require('../config/db');
 
 class Report {
 
+    // GET ALL REPORTS
     static async getAllReports() {
         const query = "SELECT * FROM reports";
         try {
@@ -12,6 +13,7 @@ class Report {
         }
     }
 
+    // CREATE NEW REPORT
     static async insertReport(report) {
         const query = "INSERT INTO reports (report_name) VALUES (?)";
         const values = [report.report_name];
@@ -20,6 +22,19 @@ class Report {
             return result;
         } catch (error) {
             throw new Error('Error inserting report');
+        }
+    }
+
+      // DELETE REPORT
+      static async deleteReport(id) {
+        const query = " DELETE FROM reports WHERE report_id = ? ";
+        const value = [id];
+        try {
+            const [result] = await connect.promise().query(query,value)
+            return result;
+        } catch (error) {
+            console.error('Error deleting Report: ', error);
+            throw new Error('Error deleting Report');
         }
     }
 }
