7c308a9777f39a2d615db1149993fdab00e80065
Ejxzdevs <ejhayyygofredo08@gmail.com>
2025-01-14 03:16:07 +0800

    added get method

--- backend/controllers/reportController.js
+++ backend/controllers/reportController.js
@@ -2,6 +2,16 @@ const ReportModel = require('../models/reportModel');
 
 class reportController {
 
+  // GET NEWS
+  getReports = async (req, res) => {
+    try {
+      const data = await ReportModel.getAllReports();
+      res.json(data); 
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  };
+
   // ADD REPORT
   addReport = async (req, res) => {
     const { report_name } = req.body;
