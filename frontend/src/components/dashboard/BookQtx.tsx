5fb92fbdd65a0641e229a20d72fb31f70f1095c3
Ejxzdevs <ejhayyygofredo08@gmail.com>
2025-01-14 22:14:45 +0800

    init

---
+++ frontend/src/components/dashboard/BookQtx.tsx
@@ -0,0 +1,42 @@
+import { Box , Typography } from '@mui/material'
+import BookIcon from '@mui/icons-material/Book';
+import { getAllBooks } from '../../services/bookApi';
+import { useEffect, useState } from 'react';
+import { Book } from '../../types/bookInterface';
+
+const BookQuantities = () => {
+    const [books, setBooks] = useState<Book[]>([]);
+    
+      useEffect(() => {
+        const fetchBooks = async () => {
+          try {
+            const data = await getAllBooks();
+            setBooks(data)
+          } catch (err) {
+            console.error('Error fetching books:', err);
+          }
+        };
+    
+        fetchBooks();
+      },[]);
+
+    const totalBooks = books.length;
+
+  return (
+    <Box className="shadow-md flex flex-row rounded-md border-2 border-[#19B37E] min-h-[120px] w-[270px]" >
+        <Box className="flex flex-col justify-center ps-7 w-[50%]" >
+            <Typography sx={{fontFamily: 'Inter', fontWeight: 500 , marginLeft: 1}} variant="h5" >
+                {totalBooks}
+            </Typography>
+            <Typography sx={{color: '#19B37E', fontFamily: 'Inter', marginLeft: 1}} variant="body1" >
+                Books
+            </Typography>
+        </Box>
+        <Box className="flex items-center justify-center ps-7 w-[50%]" >
+            <BookIcon sx={{color: '#19B37E', fontSize: 70 }} />
+        </Box>
+    </Box>
+  )
+}
+
+export default BookQuantities
