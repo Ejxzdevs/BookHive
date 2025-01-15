3f16af466ad58c4e9cf6ed02188976afefb1a72b
Ejxzdevs <ejhayyygofredo08@gmail.com>
2025-01-15 16:19:08 +0800

    change data to date

--- frontend/src/types/inquiryInterface.tsx
+++ frontend/src/types/inquiryInterface.tsx
@@ -3,7 +3,7 @@ export interface Inquiry {
     inquiry_name: string;
     inquiry_email: string;
     inquiry_status: string;
-    inquiry_data: Date;
+    inquiry_date: Date;
     inquiry_message: string;
 }
 export interface InquiryArrProps {
