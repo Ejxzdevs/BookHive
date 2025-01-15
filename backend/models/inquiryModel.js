fd8c0d06b5d87ea05de2350597a635cd466601e1
Ejxzdevs <ejhayyygofredo08@gmail.com>
2025-01-15 17:36:14 +0800

    create sql statement for update

--- backend/models/inquiryModel.js
+++ backend/models/inquiryModel.js
@@ -2,6 +2,7 @@ const connect = require('../config/db');
 
 class Request {
 
+    // GET ALL INQUIRY
     static async getAllInquiries() {
         const query = "SELECT * FROM INQUIRIES";
         try {
@@ -12,6 +13,7 @@ class Request {
         }
     }
 
+    // INSERT INQUIRY
     static async insertInquiry(inquiry) {
         const query = "INSERT INTO INQUIRIES (inquiry_name, inquiry_email, inquiry_message) VALUES (?)";
         const values = [inquiry.inquiry_name, inquiry.inquiry_email, inquiry.inquiry_message];
@@ -23,6 +25,19 @@ class Request {
         }
     }
 
+    // UPDATE STATUS INQUIRY
+    static async updateInquiryStatus(id) {
+        const query = "UPDATE inquiries SET inquiry_status = ? WHERE inquiry_id = ?";
+        const values = ['Read', id];
+        try {
+            const [result] = await connect.promise().query(query,values);
+            return result;
+        } catch (error) {
+            throw new Error('Error updating inquiry');
+        }
+    }
+
+    // DELETE INQUIRY
     static async deleteInquiry(id) {
         const query = "DELETE FROM inquiries WHERE inquiry_id = ? ";
         const value = [id];
