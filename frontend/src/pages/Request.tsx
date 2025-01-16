458e48f84fcb5745c10844c0853a17c512628ce2
Ejxzdevs <ejhayyygofredo08@gmail.com>
2025-01-17 00:26:54 +0800

    adjust label

--- frontend/src/pages/Request.tsx
+++ frontend/src/pages/Request.tsx
@@ -31,7 +31,9 @@ const Request: React.FC = () => {
     <Container disableGutters maxWidth={false} className="h-[100vh] flex flex-row p-0 m-0">
     <Sidebar />
     <Box className="flex flex-grow flex-col">
-      <Header />
+      <Box>
+        <Header />
+      </Box>
       <Box className="flex justify-between items-center pt-5 px-6">
         <Typography sx={{fontFamily: 'Inter' , fontWeight: '500'}} variant="h5" color="initial">
             Request List
